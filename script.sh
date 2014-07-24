3ad63e05b0c0b7c37a3ba7441f1e11fb8b6a433c
dobrayaufa <dobrayaufa@gmail.com>
2014-07-24 14:02:11 +0600

    Update script.sh
    Script

--- script.sh
+++ script.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 catalog=$HOME"/ScrumblerBook/"
+#cd $HOME
+#rm -rf ScrumblerBook
+cd $HOME
 git clone https://github.com/UGroup/ScrumblerBook.git
 cd ScrumblerBook
-git checkout gh-pages
-git rm -rf .
 git checkout master
 gitbook build $catalog -- github = UGroup/ScrumblerBook --title = "Scrumbler"
 git add .
 git commit -a -m "This is Scrumbler"
-mkdir new
-mv _book/* new
+mkdir $HOME/new
+cp -r _book/* $HOME/new
 catalog=$HOME"/ScrumblerBook"
 git checkout gh-pages
-mv new/* $catalog
+git rm -rf .
+cp -r $HOME/new/* $catalog
 git add .
 git commit -a -m "This is Scrumbler"
 git push origin gh-pages
+#cd $HOME
+#rm  -r  ScrumblerBook
+#rm - r new
